8643a96b9b0e4f2e33a3c22f460cb0cd4ae3f392
ll111githubac <ll111githubac@outlook.com>
2021-07-12 01:13:51 +0800

    task 2

---
+++ proj1b/Deque.java
@@ -0,0 +1,10 @@
+public interface Deque<T> {
+    void addFirst(T item);
+    void addLast(T item);
+    boolean isEmpty();
+    int size();
+    T removeFirst();
+    T removeLast();
+    T get(int index);
+    void printDeque();
+}
---
+++ proj1b/Palindrome.java
@@ -0,0 +1,9 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> wordDeque = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            wordDeque.addLast(word.charAt(i));
+        }
+        return wordDeque;
+    }
+}
