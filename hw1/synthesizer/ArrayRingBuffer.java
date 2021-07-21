6ce426da75237e187cc0662514c4271a43d9091d
ll111githubac <ll111githubac@outlook.com>
2021-07-21 21:44:31 +0800

    complete hw1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,90 @@
+package synthesizer;
+
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> implements Iterable<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        this.capacity = capacity;
+        this.rb = (T[]) new Object[capacity];
+        this.first = 0;
+        this.last = 0;
+        this.fillCount = 0;
+    }
+
+    private int circular(int index) {
+        return ((index % capacity) + capacity) % capacity;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public void enqueue(T x) {
+        if (fillCount >= capacity) {
+            throw new RuntimeException("Ring buffer overflow");
+        } else {
+            rb[last] = x;
+            last = circular(last + 1);
+            fillCount += 1;
+        }
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public T dequeue() {
+        if (fillCount == 0) {
+            throw new RuntimeException("Ring buffer underflow");
+        } else {
+            T item = rb[first];
+            first = circular(first + 1);
+            fillCount -= 1;
+            return item;
+        }
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    @Override
+    public T peek() {
+        return rb[first];
+    }
+
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+    private class ArrayRingBufferIterator implements Iterator<T> {
+        int wizPos;
+
+        ArrayRingBufferIterator() {
+            wizPos = 0;
+        }
+
+        public boolean hasNext() {
+            return wizPos < fillCount;
+        }
+
+        public T next() {
+            T nextItem = rb[circular(wizPos + first)];
+            wizPos += 1;
+            return nextItem;
+        }
+    }
+}
